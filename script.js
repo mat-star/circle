64e2fc8ed88e02dd94160594b129938cdebd1ced
mat-star <mateus.silva.jesus@escola.pr.gov.br>
2024-10-22 09:05:32 -0300

    gb

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function altCirc(){
-    const circ = document.getElementById('circulo')
+    const circ = document.getElementById('circulo');
     const corRam = '#' + Math.floor(Mat.ramdom()*16777215).toString(16);
     const nvTam = Math.floor(math.ramdom()*(200-50+1)) + 50;
     circulo.style.backgroundColor = corRam;
